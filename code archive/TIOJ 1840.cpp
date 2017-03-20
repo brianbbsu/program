eb43d1292a4a9f7a8c9ec3200511b66a3e9ef21d
蘇柏瑄(Brian Su) <brianbb.su@gmail.com>
2017-03-20 18:57:32 +0800

    TIOJ 1840 整體二分搜 unfinish

--- code archive/TIOJ 1840.cpp
+++ code archive/TIOJ 1840.cpp
@@ -40,16 +40,55 @@ template<typename _t> void pary(_t _a,_t _b){_OUTC(cerr,_a,_b);cerr<<endl;}
 #endif // brian
 //}
 
-const ll MAXn=5e4+5,MAXlg=__lg((1LL<<31))+2;
+const ll MAXn=5e4+5,MAXlg=__lg(10*MAXn)+2;
 const ll MOD=1000000007;
 const ll INF=ll(1e15);
 
-ll aryd[MAXn];
 
-vector<ll> ch[MAXlg];
-vector<ii> qr[MAXlg];
+struct chtag{
+    ll t,p,x,type;
+    chtag(ll ti,ll pi,ll xi,ll tpi):t(ti),p(pi),x(xi),type(tpi){}
+};
+struct qrtag{
+    ll t,l,r,k;
+    qrtag(ll ti,ll li,ll ri,ll ki):t(ti),l(li),r(ri),k(ki){}
+};
 
+
+ll aryd[2][MAXn];
+vector<chtag> ch[MAXlg];
+vector<qrtag> qr[MAXlg],tmpqr;
+stack<chtag> st;
 ll n,q;
+ll it=-1;
+ll ans[MAXn];
+vector<ll> uni;
+
+//bit
+ll bit[MAXn*10];
+void ins(ll x,ll k){
+    while(x<=SZ(uni))bit[x]+=k,x+=x&-x;
+}
+ll precnt(ll x){
+    ll r=0;while(x>0)r+=bit[x],x-=x&-x;return r;
+}
+
+void DC(ll now,ll l,ll r)
+{
+    if(l==r-1)
+    {
+        for(auto &k:qr[now])ans[k.t]=uni[r-1];
+        return;
+    }
+    tmpqr.clear();
+    ll chit=0;
+    for(ll &k:qr[now])
+    {
+        while(chit<SZ(ch[now])&&ch[now][chit].t<k.t){
+
+        }
+    }
+}
 
 int main()
 {
@@ -59,11 +98,53 @@ int main()
     cin>>T;
     while(T--&&cin>>n>>q)
     {
-        REP(i,n)cin>>aryd[i];
+        it=-1;
+        REP(i,MAXlg)ch[i].clear(),qr[i].clear();
+        uni.clear();
+        FILL(bit,0);
+
+        REP(i,n)cin>>aryd[0][i],uni.pb(aryd[0][i]);
+        copy_n(aryd[0],n,aryd[1]);
+
+        //input
         REP(i,q)
         {
-
+            ll t;
+            cin>>t;
+            if(t==1)
+            {
+                ll l,r,k;
+                cin>>l>>r>>k;
+                l--;r--;
+                it++;
+                qr[0].pb(qrtag(it,l,r,k));
+            }
+            else if(t==2)
+            {
+                ll x,k;
+                cin>>x>>k;
+                x--;
+                ch[0].pb(chtag(it,x,k,1));
+                ch[0].pb(chtag(it,x,aryd[0][x],-1));
+                aryd[0][x]=k;
+                uni.pb(k);
+            }
+            else
+            {
+                it++;
+                ans[it]=7122;
+            }
         }
+        sort(ALL(uni));
+        uni.resize(unique(ALL(uni))-uni.begin());
+        REP(i,n)
+        {
+            ll x=lower_bound(ALL(uni),aryd[1][i])-uni.begin()+1;
+            ins(x,1);
+        }
+        for(auto &k:ch[0])k.x=lower_bound(ALL(uni),k.x)-uni.begin()+1;
+        it(0,-1,SZ(uni)-1);
+        REP(i,it+1)cout<<ans[i]<<endl;
     }
 
 }
